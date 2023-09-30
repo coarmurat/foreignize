cbca441037d56f18705522f81e7937c5a86ba14a
Murat Coşar <coarmurat@gmail.com>
2023-09-30 23:51:30 +0300

    Header - NavMenu improvements

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,22 +1,35 @@
+"use client"
+
 import styles from './index.module.css'
 import typo from '@/typo/index.module.css'
 
-import { BackButton, HamburgerMenu } from '@/components/icons'
-import Nav from '../nav'
+import { BackButton } from '@/components/icons'
+import Nav from '@/components/nav'
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
 
 export default function Header() {
     
-
+    const [toggleNav, setToggleNav] = useState(false)
+    const handleHamburgerMenuButtonClick = () => setToggleNav( previous => !previous) 
+    
+    const router = useRouter()
+    const handleBackButtonClick = () => router.back()
+    
     return(
         <header className={styles.root}>
-            <div className={styles.backButton}>
+            <button className={styles.backButton} onClick={handleBackButtonClick}>
                 <BackButton/>
-            </div>
+            </button>
             <div className={`${styles.currentPageIndicator} ${typo.head2}`}>Landing Page</div>
-            <div className={styles.hamburgerMenuButton}>
-                <HamburgerMenu/>
+            <div className={styles.barsButtonWrapper} onClick={handleHamburgerMenuButtonClick}>
+                <button className={styles.barsButton}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </button>
             </div>
-            <Nav/>
+            <Nav toggle={toggleNav} setToggleNav={setToggleNav}/>
         </header>
     )
 }
--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,16 +1,38 @@
+"use client"
+
 import styles from './index.module.css'
 import typo from '@/typo/index.module.css'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { SetStateAction, useEffect } from 'react'
 
-export default function Nav() {
+interface NavProps {
+    toggle?:boolean,
+    setToggleNav?:React.Dispatch<SetStateAction<boolean>>
+}
+
+export default function Nav({ toggle = false, setToggleNav}:NavProps) {
     
+    const pathname = usePathname()
+    if(setToggleNav){
+        useEffect(() => setToggleNav( () => false ),[pathname])
+    }
+    
+
     return(
-        <nav className={styles.root}>
+        <nav className={`${styles.root} ${toggle ? '' : styles.hide }`}>
             <ul>
-                <li className={typo.head2}>Landing Page</li>
-                <li className={typo.head2}>Text List</li>
-                <li className={typo.head2}>How Works</li>
+                <div className={styles.primaryLinks}>
+                    <li><Link className={typo.head2} href='/'> Landing Page </Link></li>
+                    <li><Link className={typo.head2} href='/list'> Text List </Link></li>
+                    <li><Link className={typo.head2} href='/how'> How Works </Link></li>
+                </div>
+                <div className={styles.secondaryLinks}>
+                    <li><Link className={typo.head2} href='/login'> Login </Link></li>
+                    <li><Link className={typo.head2} href='/register'> Register </Link></li>
+                </div>
             </ul>
-            <div></div>
+            <div onClick={ () => setToggleNav ? setToggleNav(() => false):null }></div>
         </nav>  
     )
 }
